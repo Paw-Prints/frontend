0ff3ccf5a0b846ff3c1a6c714f922f03c483c8ca
lesley <lesleybanadzem2017@gmail.com>
2019-10-02 15:47:02 -0700

    Created a form and setStates on submit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import "antd/dist/antd.css"
+// import "antd/dist/antd.css"
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import HomePage from './HomePage/HomePage';
 import NavBar from './NavBar/NavBar';
