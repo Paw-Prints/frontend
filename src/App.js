497698f2936324ce0293b6d8251ca96140e3691a
Julie Jonak <juliejonak10@gmail.com>
2019-10-02 11:17:22 -0700

    Sets up initial repository

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          PawPrints is live! Check back soon for more...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
