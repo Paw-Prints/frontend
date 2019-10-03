4cec93bcaf26766bc4fbf4a7625aed34ab1eb050
josephriosio <blankworldwide@gmail.com>
2019-10-04 02:05:52 +0300

    added small fixes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 // import "antd/dist/antd.css"
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import HomePage from './HomePage/HomePage';
-import NavBar from './NavBar/NavBar';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "./HomePage/HomePage";
+import NavBar from "./NavBar/NavBar";
 
 function App() {
-
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/home" component={HomePage} />
-          <Route path="/" component={NavBar} />
+          <Route path="/" component={HomePage} />
         </Switch>
       </Router>
     </div>
--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,69 +1,72 @@
-import React, { Component } from 'react'
-import HomePageForm from '../Components /HomePageForm.js/HomePageForm';
+import React, { Component } from "react";
+import HomePageForm from "../Components/HomePageForm.js/HomePageForm";
 // var request = require("request");
 var axios = require("axios");
 // const serverurl=`http://localhost:3001`
 
 export default class HomePage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            breed : '',
-            location :  '',
-            city: '',
-            state: ''
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      breed: "",
+      location: "",
+      city: "",
+      state: ""
+    };
+  }
 
-    componentDidMount() {
-        
+  componentDidMount() {}
+  handleChange = e => {
+    const {
+      target: { name, value }
+    } = e;
+    this.setState({ [name]: value });
+  };
+  handleSelect = e => {
+    e.preventDefault();
+    this.setState({ breed: e.target.value });
+  };
+  handleCityState = e => {
+    e.preventDefault();
+    this.setState({ state: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    let body = {};
+    if (this.state.location.length > 0) {
+      body = {
+        location: this.state.location,
+        breed: this.state.breed
+      };
+    } else if (this.state.city.length > 0) {
+      body = {
+        breed: this.state.breed,
+        location: `${this.state.city + ", " + this.state.state}`
+      };
     }
-    handleChange =(e) =>{
-        const { target: { name, value } } = e;
-        this.setState({ [name]: value })
-    }
-    handleSelect = e =>{
-        e.preventDefault()
-        this.setState({ breed: e.target.value })
-    } 
-    handleCityState = e => {
-        e.preventDefault()
-        this.setState({ state: e.target.value })
-    }
-    handleSubmit = e =>{
-        e.preventDefault();
-        let body = {}
-        if(this.state.location.length > 0){
-                body = { 
-                    location: this.state.location,
-                     breed: this.state.breed
-                    }
-            
-        } else if (this.state.city.length > 0 ){
-            body = {
-                breed: this.state.breed,
-                 location: `${this.state.city + ', ' + this.state.state}`}
-        }
-                console.log(body)
-        axios.post('https://paw-prints.herokuapp.com/api/',        body
-        ).then(res=>{
-                console.log(res)
-            })
-            .catch(err=> { throw Error })
-    }
-    render() {
-        return (
-            <div>
-                <HomePageForm 
-                    breed = { this.state.breed }
-                    city = {this.state.city}
-                    location = { this.state.location}
-                    handleChange = { this.handleChange }
-                    handleSelect = { this.handleSelect }
-                    handleSubmit = { this.handleSubmit }
-                    handleCityState = {this.handleCityState }
-                />
-            </div>
-        )
-    }
+    console.log(body);
+    axios
+      .post("https://paw-prints.herokuapp.com/api/", body)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        throw Error;
+      });
+  };
+  render() {
+    return (
+      <div>
+        <HomePageForm
+          breed={this.state.breed}
+          city={this.state.city}
+          location={this.state.location}
+          handleChange={this.handleChange}
+          handleSelect={this.handleSelect}
+          handleSubmit={this.handleSubmit}
+          handleCityState={this.handleCityState}
+        />
+      </div>
+    );
+  }
 }
