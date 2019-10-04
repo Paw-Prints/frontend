bcb3aea146078328a540da60bdf9e11970302b48
josephriosio <blankworldwide@gmail.com>
2019-10-03 21:54:37 -0700

    added some style changes to pet info page

--- src/DogProfile/DogProfile.js
+++ src/DogProfile/DogProfile.js
@@ -1,30 +1,37 @@
-import React, { Component } from 'react'
-import { token } from "../token"
+import React from "react";
+import Header from "../Components /Header/Header";
+import declawed from "../Images/Attributes/declawed.png";
+import houseTrained from "../Images/Attributes/houseTrained.png";
+import vaccinated from "../Images/Attributes/vaccination-color.png";
+import neutered from "../Images/Attributes/neutered.png";
+import speicalNeeds from "../Images/Attributes/special_need_color.png";
 
-export default class DogProfile extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            token: `${token}`,
-            dogid: "",
-        }
-    }
-    componentDidMount(){
-        console.log("check this out!! ",((this.props.location.state||{}).generalbreeddata||{}));
-        console.log("and this !! ",((this.props.location.state||{}).item||{}));
-        let dogid =((this.props.match||{}).params||{}).dogid;
-        console.log("CHECK OUT THE DOG ID",dogid);
-        this.setState({
-            dogid: dogid
-        })
-    }
+class DogProfile extends React.Component {
+  state = {};
 
-    render() {
-        
-        return (
-            <div>
-                CHECK OUT THE DOG ID ${this.state.dogid}
-            </div>
-        )
-    }
+  render() {
+    const { pet, generalbreeddata } = this.props.location.state;
+    const { images, name, description, breeds, attributes, contact, age } = pet;
+    console.log(pet, generalbreeddata);
+
+    return (
+      <div>
+        <Header />
+        <div>
+          <img src={images[0].medium} />
+          <h1>{name}</h1>
+          <p>{description}</p>
+        </div>
+        <div>
+          {attributes.declawed ? <img src={declawed} /> : null}
+          {attributes.house_trained ? <img src={houseTrained} /> : null}
+          {attributes.shots_current ? <img src={vaccinated} /> : null}
+          {attributes.spayed_neutered ? <img src={neutered} /> : null}
+          {attributes.special_needs ? <img src={speicalNeeds} /> : null}
+        </div>
+      </div>
+    );
+  }
 }
+
+export default DogProfile;
