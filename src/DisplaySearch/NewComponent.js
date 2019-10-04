29c29f45a6a9d65ea4034d356ae7815d3344df75
josephriosio <blankworldwide@gmail.com>
2019-10-03 20:56:30 -0700

    added some basic layout for dog page

--- src/DisplaySearch/NewComponent.js
+++ src/DisplaySearch/NewComponent.js
@@ -1,14 +1,36 @@
 import React from "react";
-import PropTypes from "prop-types";
+import Header from "../Components/Header/Header";
+import declawed from "../Images/Attributes/declawed.png";
+import houseTrained from "../Images/Attributes/houseTrained.png";
+import vaccinated from "../Images/Attributes/vaccination-color.png";
+import neutered from "../Images/Attributes/neutered.png";
+import speicalNeeds from "../Images/Attributes/special_need_color.png";
 
 class NewComponent extends React.Component {
   state = {};
 
   render() {
     const { pet, generalbreeddata } = this.props.location.state;
+    const { images, name, description, breeds, attributes, contact, age } = pet;
     console.log(pet, generalbreeddata);
 
-    return <div>hi</div>;
+    return (
+      <div>
+        <Header />
+        <div>
+          <img src={images[0].medium} />
+          <h1>{name}</h1>
+          <p>{description}</p>
+        </div>
+        <div>
+          {attributes.declawed ? <img src={declawed} /> : null}
+          {attributes.house_trained ? <img src={houseTrained} /> : null}
+          {attributes.shots_current ? <img src={vaccinated} /> : null}
+          {attributes.spayed_neutered ? <img src={neutered} /> : null}
+          {attributes.special_needs ? <img src={speicalNeeds} /> : null}
+        </div>
+      </div>
+    );
   }
 }
 
