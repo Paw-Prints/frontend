0d5906fe8686e205e2e984cdf7445cfaa9f42166
Julie Jonak <juliejonak10@gmail.com>
2019-10-04 08:32:34 -0700

    Updates dog search results

--- src/DisplaySearch/DisplaySearch.js
+++ src/DisplaySearch/DisplaySearch.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
-import { Spin, List } from "antd";
+import { Spin, List, Popover, Button } from "antd";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Header from "../Components /Header/Header";
-import DogProfile from "../DogProfile/DogProfile";
+import declawed from "../Images/Attributes/declawed.png";
+import houseTrained from "../Images/Attributes/houseTrained.png";
+import vaccinated from "../Images/Attributes/vaccination-color.png";
+import neutered from "../Images/Attributes/neutered.png";
+import specialNeeds from "../Images/Attributes/special_need_color.png";
 
 export default class DisplaySearch extends Component {
   constructor(props) {
@@ -15,34 +19,40 @@ export default class DisplaySearch extends Component {
     };
   }
   componentDidMount() {
-    const generalbreeddata = this.props.location.state.responseObj.costs;
-
     this.setState({
+      loading: false,
       pets: this.props.location.state.responseObj.pets,
-      generalbreeddata: generalbreeddata,
-      loading: false
+      generalbreeddata: this.props.location.state.responseObj.costs,
     });
-  }
+  };
 
   render() {
-    // Issues with this rendering before props are filled
+    let costHeader
 
-    let pets = this.state.pets || [];
-    let totalCost = this.state.generalbreeddata
+    console.log(this.state.pets)
 
-    console.log(this.state.generalbreeddata)
+    if(this.state.loading){
+      costHeader = (<div>
+        <SpinnerDiv>
+            <Spin tip="Loading..." size="large" />
+        </SpinnerDiv>
+    </div>)
+    } else {
+      costHeader = (
+        <SpinnerDiv>
+          <h2>Estimated Annual Costs: {this.state.generalbreeddata.annual.total.withInsurance} </h2>
+        </SpinnerDiv>
+      )
+    }
 
     return (
       <div>
         <Header style={{ zIndex: "3"}} />
-
-        <div style={{minWidth: "400px", width: "70%", margin: "auto", marginTop: "60px", zIndex: "0"}}>
-
-          <h2>Estimated Annual Costs: </h2>
-          {/* { totalCost ? <h2>Estimated Annual Costs: {totalCost.annual.total.withInsurance} </h2> : null } */}
+        <div style={{minWidth: "400px", width: "70%", margin: "auto", marginTop: "60px", zIndex: "0", marginBottom: "-60px"}}>
+          {costHeader}
         </div>
 
-      <div style={{minWidth: "400px", width: "70%", margin: "auto", marginTop: "60px", zIndex: "0"}}>
+      <div style={{minWidth: "400px", width: "70%", margin: "auto", marginTop: "60px"}}>
 
         <List
           itemLayout="vertical"
@@ -58,6 +68,7 @@ export default class DisplaySearch extends Component {
           }}
 
           dataSource={this.state.pets}
+
           renderItem={item => (
             <List.Item style={{flexDirection: "row-reverse"}}
               key={item.title}
@@ -75,23 +86,70 @@ export default class DisplaySearch extends Component {
                     {item.title}
                   </a>
                 }
-                description={item.description}
               />
               {
-                <p>
-                  <Link
-                    to={{
-                      pathname: `/display/${item.name}`,
-                      state: {
-                        generalbreeddata: this.state.generalbreeddata,
-                        pet: item,
-                        pets: this.state.pets
-                      }
-                    }}
-                  >
-                    Learn More about {item.name}
-                  </Link>
-                </p>
+                <div>
+                  <h2>{item.name}</h2>
+                  <p>{(item.distance).toFixed(2)} miles from you</p>
+
+                  <div style={{ display: "flex", width: "70%", margin: "auto", marginBottom: "5%", justifyContent: "center" }}>
+                    {item.attributes.spayed_neutered ? (
+                      <div> 
+                        <Popover placement="topLeft" content={<p>Spayed/Neutered</p>} arrowPointAtCenter>
+                          <img src={neutered} style={{ height: "6vh", width: "auto", marginRight: "10px"  }} alt="neutered" />
+                        </Popover>
+                      </div>
+                    ) : null}
+
+                    {item.attributes.declawed ? (
+                      <div> 
+                        <Popover placement="topLeft" content={<p>De-clawed</p>} arrowPointAtCenter>
+                          <img src={declawed} style={{ height: "6vh", width: "auto", marginRight: "10px"  }} alt="declawed" />
+                        </Popover>
+                      </div>
+                    ) : null}
+
+                    {item.attributes.house_trained ? (
+                      <div> 
+                        <Popover placement="topLeft" content={<p>House Trained</p>} arrowPointAtCenter>
+                          <img src={houseTrained} style={{ height: "6vh", width: "auto", marginRight: "10px"  }} alt="house trained" />
+                        </Popover>
+                      </div>
+                    ) : null}
+
+                    {item.attributes.shots_current ? (
+                      <div> 
+                        <Popover placement="topLeft" content={<p>Vaccinated</p>} arrowPointAtCenter>
+                          <img src={vaccinated} style={{ height: "6vh", width: "auto", marginRight: "10px"  }} alt="vaccinated" />
+                        </Popover>
+                      </div>
+                    ) : null}
+
+                    {item.attributes.special_needs ? (
+                      <div> 
+                        <Popover placement="topLeft" content={<p>Special Needs</p>} arrowPointAtCenter>
+                          <img src={specialNeeds} style={{ height: "6vh", width: "auto" }} alt="special needs" />
+                        </Popover>
+                      </div>
+                    ) : null}
+                  </div>
+
+                  <Button type="primary">
+                    <Link
+                      to={{
+                        pathname: `/display/${item.name}`,
+                        state: {
+                          generalbreeddata: this.state.generalbreeddata,
+                          pet: item,
+                          pets: this.state.pets
+                        }
+                      }}
+                    >
+                      Learn More about {item.name}
+                    </Link>
+                  </Button>
+                  
+                </div>
               }
             </List.Item>
           )}
